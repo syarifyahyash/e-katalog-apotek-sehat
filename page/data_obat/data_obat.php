c5688fa328151e894d92bf9f084a173e93bedd58
Syarif Yahya <syarifyahya.pmk@gmail.com>
2023-07-06 05:58:41 +0700

    update
    ngubah urutan data obat

--- page/data_obat/data_obat.php
+++ page/data_obat/data_obat.php
@@ -7,11 +7,11 @@ default:
 <head>
 	<link rel="stylesheet" href="css/style-obat.css">
 </head>
-<h1 class="text-center" style="margin-top:60px;">Katalog Obat</h1>
+<h1 class="judul text-center" style="margin-top:60px;">Katalog Obat</h1>
 <hr/>
 <div class="row">
 	<div class="col-sm">
-	  <h4 class="float-left">Data Obat</h4> 
+	  <h4 class="sub-judul float-left">Data Obat</h4> 
 	  	<a href="?menu=data_obat&submenu=tambah" class="btn-add btn btn-success float-right"  style="border-radius: 5px;" id="addData">Tambah Data Obat</a>
 	  </form>
 	 <div class="table-responsive">
@@ -22,9 +22,9 @@ default:
 			<th class="col-sm-2">Nama Obat</th>
 			<th >Kategori</th>
 			<th >Indikasi</th>
+			<th >Harga</th>
 			<th >Stok</th>
 			<th >Tgl Expired</th>
-			<th >Harga</th>
 			<th class="text-center">Edit</th>
 			<th class="text-center">Delete</th>
 		</tr>
@@ -56,9 +56,9 @@ default:
 			<td><?= $d['nama_obat']; ?></td>
 			<td><?= $jenis_obat_array[$d['kategori_obat']]; ?></td>
 			<td><?= $d['indikasi']; ?></td>
+			<td><?= $d['harga']; ?></td>
 			<td><?= $d['stok_obat']; ?></td>
 			<td><?= $d['tanggal_expired']; ?></td>
-			<td><?= $d['harga']; ?></td>
 			<td>
 				<a class="btn-edit btn btn-outline-success btn-sm text-center" style="border-radius: 5px;" id="edit" href="?menu=data_obat&submenu=edit&id=<?= $d['id_obat'];?>">Edit</a>
 			</td>
